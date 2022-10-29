ba7f1fc1e543c70699a2d55d80464805a44ef6e1
Andrews <andyboa75@gmail.com>
2022-10-29 23:13:43 +0000

    modified index.php

--- index.php
+++ index.php
@@ -5,17 +5,13 @@
  * @package Discova
  */
 
+ get_header();
  ?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress theme</title>
- </head>
- <body>
-    <h1> Hello wordpress </h1>
- </body>
- </html>
+<div class="content">
+ <h1> WordPress theme </h1>
+</div>
+
+<?php 
+
+get_footer();
